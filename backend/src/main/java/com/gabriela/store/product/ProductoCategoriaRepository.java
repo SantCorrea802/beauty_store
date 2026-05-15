51e443906ac8f19b0b73518115fa49f1796aa0f0
SantCorrea802 <santiago.correa7@udea.edu.co>
2026-05-14 23:18:35 -0500

    se agrega metodo para buscar relaciones por id producto

--- backend/src/main/java/com/gabriela/store/product/ProductoCategoriaRepository.java
+++ backend/src/main/java/com/gabriela/store/product/ProductoCategoriaRepository.java
@@ -2,6 +2,9 @@ package com.gabriela.store.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductoCategoriaRepository extends JpaRepository<ProductoCategoria, Long> {
     boolean existsByProducto_IdProductoAndCategoria_IdCategoria(Long idProducto, Long idCategoria);
+    List<ProductoCategoria> findByProducto_IdProducto(Long idProducto);
 }
