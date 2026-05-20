d73819366de4925336a9cb9ad066d9e6ec604824
SantCorrea802 <santiago.correa7@udea.edu.co>
2026-05-19 22:00:46 -0500

    modificando config para que permita put

--- backend/src/main/java/com/gabriela/store/config/SecurityConfig.java
+++ backend/src/main/java/com/gabriela/store/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.gabriela.store.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,15 +10,14 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        System.out.println(">>> CUSTOM SECURITY CONFIG LOADED <<<");
+
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/categories/**").permitAll()
                         .requestMatchers("/api/products/**").permitAll()
-
-                        // Temporal para desarrollo local.
-                        .requestMatchers(HttpMethod.POST, "/api/admin/products").permitAll()
-
+                        .requestMatchers("/api/admin/products/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .build();
