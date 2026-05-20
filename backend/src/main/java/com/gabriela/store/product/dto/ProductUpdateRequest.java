1590a7e3c08678ed49b443178cc3892e9f48b97b
SantCorrea802 <santiago.correa7@udea.edu.co>
2026-05-19 22:01:39 -0500

    crear el record de un producto cuando se actualice

---
+++ backend/src/main/java/com/gabriela/store/product/dto/ProductUpdateRequest.java
@@ -0,0 +1,25 @@
+package com.gabriela.store.product.dto;
+
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record ProductUpdateRequest(
+        @NotBlank(message = "El nombre del producto es obligatorio.")
+        @Size(max = 160, message = "El nombre no puede superar los 160 caracteres.")
+        String nombre,
+
+        @NotNull(message = "El precio del producto es obligatorio.")
+        @DecimalMin(value = "0.0", inclusive = true, message = "El precio no puede ser negativo.")
+        BigDecimal precio,
+
+        @Size(max = 3000, message = "La descripción no puede superar los 3000 caracteres.")
+        String descripcion,
+
+        @Size(max=100, message = "La marca no puede superar los 100 caracteres.")
+        String marca,
+
+        @NotEmpty(message = "El producto debe tener al menos una categoría.")
+        List<Long> categoriaIds
+) {}
